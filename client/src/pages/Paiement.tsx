8fcaa888880a4e7bc46a16214f9e29d90df57898
Mariondevdebutante <marion.baraer292@gmail.com>
2026-03-07 09:51:56 +0100

    Front paiement wip

--- client/src/pages/Paiement.tsx
+++ client/src/pages/Paiement.tsx
@@ -7,8 +7,49 @@ function Paiement() {
   return (
     <div className="page-container">
       <Header />
-      <h1>Page de paiement</h1>
-      <Navbar />
+      <div className="paiement-container">
+        <div className="adress-section">
+          <p className="section-title">Adresse de livraison</p>
+          <p className="section-content">123 rue de la paix, 75000 Paris</p>
+        </div>
+
+        <div className="order-sum">
+          <div className="first-article">
+            <p className="section-title">Article 1</p>
+            <p className="section-content">T-shirt rouge - 20€</p>
+          </div>
+          <div className="first-article">
+            <p className="section-title">Article 1</p>
+            <p className="section-content">T-shirt rouge - 20€</p>
+          </div>
+          <div className="first-article">
+            <p className="section-title">Article 1</p>
+            <p className="section-content">T-shirt rouge - 20€</p>
+          </div>
+          <div className="first-article">
+            <p className="section-title">Article 1</p>
+            <p className="section-content">T-shirt rouge - 20€</p>
+          </div>
+        </div>
+
+        <div className="total-section">
+          <p className="section-total">Sous-total</p>
+          <p className="section-total">80€</p>
+          <p className="section-total">Frais de livraison</p>
+          <p className="section-total">5€</p>
+          <p className="section-total">Total</p>
+          <p className="section-total">85€</p>
+        </div>
+
+        <div className="payment-section">
+          <p className="section-title">Methode de paiement</p>
+          <p className="section-content">Carte bancaire</p>
+        </div>
+
+        <div className="confirm-section"></div>
+
+        <Navbar />
+      </div>
     </div>
   );
 }
