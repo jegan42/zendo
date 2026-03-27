67173f3d091063c13f844a65b63366c018f34090
JCGAN <aznchristophe@gmail.com>
2026-03-27 09:33:57 +0100

    fix cors for local frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,6 +12,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://localhost:3000",
+            "https://zendo-sandy.vercel.app",
             // "https://ton-front.vercel.app",
         ],
         credentials: true,
